ceca63d0cd08c333895d891b1b1b9a44d5d60645
Fillipe <fillipe10@yahoo.com.br>
2025-12-11 21:43:39 -0300

    Alteração no layout do UI

--- gui.rb
+++ gui.rb
@@ -3,16 +3,15 @@ include Fox
 
 class AutomationApp < FXMainWindow
   def initialize(app)
-    super(app, "Robô de Automação", width: 400, height: 200)
+    super(app, "Automação EPROC", width: 400, height: 200)
 
     # Layout principal
     frame = FXVerticalFrame.new(self, LAYOUT_FILL_X | LAYOUT_FILL_Y)
 
-    # Texto de instrução
-    FXLabel.new(frame, "Clique no botão para iniciar a automação:")
-
     # Botão para iniciar a automação
-    FXButton.new(frame, "Iniciar Automação", nil, nil, 0, FRAME_THICK | FRAME_RAISED) do |button|
+    FXButton.new(frame, "Baixar Boletos", nil, nil, 0, FRAME_THICK | FRAME_RAISED | LAYOUT_CENTER_X | LAYOUT_CENTER_Y) do |button|
+      button.width = (button.width * 8).to_i
+      button.height = (button.height * 8).to_i
       button.connect(SEL_COMMAND) do
         system("ruby run.rb")
       end
